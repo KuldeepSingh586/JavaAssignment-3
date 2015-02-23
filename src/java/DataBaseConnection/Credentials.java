8d1987ceacc18d4246edd9aa30f7ad36db1e7b61
Kuldeep Singh <c0648442@mylambton.ca>
2015-02-22 19:09:22 -0500

    Made dataBase connection

--- src/java/DataBaseConnection/Credentials.java
+++ src/java/DataBaseConnection/Credentials.java
@@ -6,10 +6,33 @@
 
 package DataBaseConnection;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Kuldeep
  */
 public class Credentials {
-    
+        /**
+     * Provides a Connection to the IPRO sample DB
+     * @return - the connection object or null if a connection failed
+     */
+    public static Connection getConnection() {
+         Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/c0648442";
+            String user = "root";
+            String pass = "";
+            conn = DriverManager.getConnection(jdbc, user, pass);
+            String query = "SELECT * FROM product";
+
+        }
+            catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Credentials.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn; 
+    }
 }
